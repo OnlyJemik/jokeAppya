02680305381186d52608886953f3b9297dda1e00
OnlyJemik <wiktor.sibinski0@gmail.com>
2024-03-22 20:14:18 +0100

    added issue 1

---
+++ src/main/java/pl/akademiaspecjalistowit/jokeapp/issue1.java
@@ -0,0 +1,8 @@
+package pl.akademiaspecjalistowit.jokeapp;
+
+public class issue1 {
+    public static void main(String[] args) {
+            System.out.println("Dlaczego programiści nie lubią natury?");
+        System.out.println("Bo mają problem z pamięcią cache!");
+    }
+}
